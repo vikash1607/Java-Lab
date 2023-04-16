6504590bdac20c7710b996ac52151833c3b1b3bf
Vikash Kumar <126924311+vikash1607@users.noreply.github.com>
2023-04-17 00:58:55 +0530

    Add files via upload

---
+++ LAB_03/Objectcounter.java
@@ -0,0 +1,23 @@
+/* 5. WAP to count number of objects created in a program using static data member and constructor in
+a class called ObjectCounter*/
+
+
+public class Objectcounter {
+    private static int count = 0;
+
+    public Objectcounter() {
+        ++count;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Objectcounter obj1 = new Objectcounter();
+        Objectcounter obj2 = new Objectcounter();
+        Objectcounter obj3 = new Objectcounter();
+
+        System.out.println("Number of objects created: " + Objectcounter.getCount());
+    }
+}
